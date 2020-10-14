b7ad73a997b7b57056014fea45a8e9df95e5464e
Manish Patel <manishpatel@esriaustralia.com.au>
2020-10-14 14:03:17 +1100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ArcGIS_System_Profiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBox_agsServerhostname = new System.Windows.Forms.TextBox();
+            this.txtBox_agsEnterprisehostname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.agsServerlistBox = new System.Windows.Forms.ListBox();
+            this.agsEnterpriselistBox = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This application is developed for performing ArcGIS System profiling to generate " +
+    "report for ArcGIS Enterprise";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Parameters";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ArcGIS Server endpoint";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "ArcGIS Enterprise endpoint";
+            // 
+            // txtBox_agsServerhostname
+            // 
+            this.txtBox_agsServerhostname.Location = new System.Drawing.Point(221, 79);
+            this.txtBox_agsServerhostname.Name = "txtBox_agsServerhostname";
+            this.txtBox_agsServerhostname.Size = new System.Drawing.Size(375, 20);
+            this.txtBox_agsServerhostname.TabIndex = 2;
+            // 
+            // txtBox_agsEnterprisehostname
+            // 
+            this.txtBox_agsEnterprisehostname.Location = new System.Drawing.Point(221, 107);
+            this.txtBox_agsEnterprisehostname.Name = "txtBox_agsEnterprisehostname";
+            this.txtBox_agsEnterprisehostname.Size = new System.Drawing.Size(375, 20);
+            this.txtBox_agsEnterprisehostname.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(603, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get Services";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(602, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Get Items";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(15, 196);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(772, 422);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.agsServerlistBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(764, 396);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "ArcGIS Server";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.agsEnterpriselistBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(764, 396);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "ArcGIS Portal";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Details";
+            // 
+            // agsServerlistBox
+            // 
+            this.agsServerlistBox.FormattingEnabled = true;
+            this.agsServerlistBox.Location = new System.Drawing.Point(7, 7);
+            this.agsServerlistBox.Name = "agsServerlistBox";
+            this.agsServerlistBox.Size = new System.Drawing.Size(751, 381);
+            this.agsServerlistBox.TabIndex = 0;
+            // 
+            // agsEnterpriselistBox
+            // 
+            this.agsEnterpriselistBox.FormattingEnabled = true;
+            this.agsEnterpriselistBox.Location = new System.Drawing.Point(7, 8);
+            this.agsEnterpriselistBox.Name = "agsEnterpriselistBox";
+            this.agsEnterpriselistBox.Size = new System.Drawing.Size(751, 381);
+            this.agsEnterpriselistBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 630);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBox_agsEnterprisehostname);
+            this.Controls.Add(this.txtBox_agsServerhostname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ArcGIS System Profiler";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBox_agsServerhostname;
+        private System.Windows.Forms.TextBox txtBox_agsEnterprisehostname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox agsServerlistBox;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox agsEnterpriselistBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
