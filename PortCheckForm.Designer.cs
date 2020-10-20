3c53577c52a045813b242bb8c2dfe130eecd38a1
Manish Patel EsriAU <manishpatel@esriaustralia.com.au>
2020-10-20 13:37:28 +1100

    port check with theme and styling

--- PortCheckForm.Designer.cs
+++ PortCheckForm.Designer.cs
@@ -28,69 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btn_PerformPortCheck = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 42);
             this.label1.MaximumSize = new System.Drawing.Size(500, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(65, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // panel1
             // 
+            this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(13, 42);
+            this.panel1.Location = new System.Drawing.Point(13, 64);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(759, 339);
+            this.panel1.Size = new System.Drawing.Size(1071, 364);
             this.panel1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(57, 0);
+            this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(89, 5);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(127, 22);
             this.label2.TabIndex = 2;
             this.label2.Text = "Port Number";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 0);
+            this.label3.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 5);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.Size = new System.Drawing.Size(69, 22);
             this.label3.TabIndex = 2;
             this.label3.Text = "Status";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(22, 434);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1059, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 14);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(15, 450);
+            this.panel4.TabIndex = 25;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(1091, 14);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(15, 450);
+            this.panel2.TabIndex = 26;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1106, 14);
+            this.panel3.TabIndex = 27;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(0, 464);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1106, 14);
+            this.panel5.TabIndex = 28;
+            // 
+            // btn_PerformPortCheck
+            // 
+            this.btn_PerformPortCheck.BackColor = System.Drawing.Color.DimGray;
+            this.btn_PerformPortCheck.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_PerformPortCheck.ForeColor = System.Drawing.Color.Black;
+            this.btn_PerformPortCheck.Location = new System.Drawing.Point(25, 24);
+            this.btn_PerformPortCheck.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_PerformPortCheck.Name = "btn_PerformPortCheck";
+            this.btn_PerformPortCheck.Size = new System.Drawing.Size(173, 30);
+            this.btn_PerformPortCheck.TabIndex = 30;
+            this.btn_PerformPortCheck.Text = "Perform Port checks";
+            this.btn_PerformPortCheck.UseVisualStyleBackColor = false;
+            this.btn_PerformPortCheck.Click += new System.EventHandler(this.btn_PerformPortCheck_Click);
             // 
             // PortCheckForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(79)))), ((int)(((byte)(105)))));
+            this.ClientSize = new System.Drawing.Size(1106, 478);
+            this.Controls.Add(this.btn_PerformPortCheck);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
             this.Name = "PortCheckForm";
             this.Text = "PortCheckForm";
             this.panel1.ResumeLayout(false);
@@ -100,11 +179,16 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btn_PerformPortCheck;
     }
 }
--- PortCheckForm.cs
+++ PortCheckForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,20 +18,57 @@ namespace ArcGIS_System_Profiler
         public PortCheckForm()
         {
             InitializeComponent();
+            label1.Text = "";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "";
-            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
-            IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
 
-            foreach (IPEndPoint endPoint in ipEndPoints)
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = progressBar1.Value + 2;
+            if (progressBar1.Value > 99)
             {
-                //label1.Text = label1.Text + "\n" + endPoint.Port + " in Use";
-                label1.Text = label1.Text + "in Use : " + endPoint.Port + "\n";
+                label1.Text = "";
+                IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+                IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+
+                foreach (IPEndPoint endPoint in ipEndPoints)
+                {
+                    //label1.Text = label1.Text + "\n" + endPoint.Port + " in Use";
+                    try
+                    {
+                        using (TcpClient tcpClient = new TcpClient())
+                        {
+                            tcpClient.Connect("127.0.0.1", endPoint.Port);
+                            label1.Text = label1.Text + "in Use : " + endPoint.Port + " - Port open\n";
+                            //label1.Text = label1.Text + "in Use : " + endPoint.Port + " -< span style = \"color: green; \" >  Port open</ span >\n";
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        label1.Text = label1.Text + "in Use : " + endPoint.Port + " - Port closed\n";
+                    }
+                }
+                timer1.Enabled = false;
+
             }
 
-        } 
+
+            if (progressBar1.Value == 100)
+            {
+                progressBar1.Hide();
+            }
+        }
+
+        private void btn_PerformPortCheck_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
     }
 }
